32c7d05a3d69c11fc9105c635b5dc1164f6aee4c
Shanda Shaw <srh0029@hotmail.com>
2023-11-21 11:48:18 -0600

    header color & commented out GA logo

--- frontend/hackathon-thon/src/components/Header.jsx
+++ frontend/hackathon-thon/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../srh.css'
+// import GA from '../assets/GA.png';
 
 export default function Header () {
     return (
         <header>
             <h1>sAGe</h1>
-            <img></img>
+            {/* <div id="logo">
+                <img src={GA} className='GAlogo' alt="" />
+            </div> */}
         </header>
     )
 }
