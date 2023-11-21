d211fcc50fa0092d2c35fdfa33d9c0ff57749ea5
Robert <rcastor0000@gmail.com>
2023-11-21 09:11:43 -0600

    matches page -progress made

--- frontend/hackathon-thon/src/components/Main.jsx
+++ frontend/hackathon-thon/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import Matches from './Matches'
 
 export default function Main(props) {
     return (
         <div>
             <Routes>
                 <Route path = '/' element = {<Home/>} />
+                <Route path='/matches' element={<Matches />} />
             </Routes>
         </div>
     )
