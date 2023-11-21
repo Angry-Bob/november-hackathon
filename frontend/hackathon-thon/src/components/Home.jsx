335764d7d3ee3347eee4f68b425f13386874e0e8
Shanda Shaw <srh0029@hotmail.com>
2023-11-21 08:46:16 -0600

    rendering homepage

--- frontend/hackathon-thon/src/components/Home.jsx
+++ frontend/hackathon-thon/src/components/Home.jsx
@@ -3,7 +3,16 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div>
-            <h1>Welcome Home</h1>
+            <div>
+                <h2>Sign Up</h2>
+            </div>
+            <div>
+                <h3>Email</h3>
+                <input type = "email" id = "email"></input>
+                <h3>Password</h3>
+                <input type = "password" id = "password"></input>
+                <button id = "submit">Save</button>
+            </div>
         </div>
     )
 }
